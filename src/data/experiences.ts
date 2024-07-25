768b1e2d286dcfeb371fff290c0cad824ff9c243
Kheireddine Boukhatem <105202227+kheireddinebou@users.noreply.github.com>
2024-07-25 17:16:55 +0100

    update resume and digital home experience description

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -8,10 +8,10 @@ export const experiences: Experience[] = [
       "Design and implement a comprehensive admin dashboard for the company team, providing detailed business insights and simplifying management processes.",
       "Deliver a SaaS app for mobile charging that is fully secured, easy to use, and includes subscription management features.",
       "Develop an app to streamline bill payments and create a separate app for efficient e-payment solutions.",
-      "Improve app performance by 10% by resolving critical issues stemming from improper coding practices, leading to unnecessary rerenders and app freezes.",
+      "Improved app loading time by over 10% by resolving critical issues related to improper coding practices, eliminating unnecessary rerenders and app freezes.",
       "Discuss client requirements to add new features that enhance user experience.",
       "Utilize data visualization techniques to present complex business data clearly and effectively.",
-      "Fixing production issues.",
+      "Identified and resolved production issues, ensuring smooth functionality and optimal user experience.",
     ],
     technologies: [
       "Front-End Development",
